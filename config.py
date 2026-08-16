f050986b29dd3172c610d8662f62e975eb367b43
zorpix1 <zorpix1@users.noreply.github.com>
2026-08-16 15:16:59 -0700

    Configure GitHub Pages URLs

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
+ghPagesURL = "https://zorpix1.github.io/tiktok-rss-flat/"
+ghRawURL = "https://raw.githubusercontent.com/zorpix1/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
